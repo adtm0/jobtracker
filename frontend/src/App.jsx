a317903518a5805af3dc6ca79f6addabd49c5d20
adtm0 <malanog.aubrey@gmail.com>
2026-04-19 17:35:07 +0800

    add navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,18 @@ import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import Jobs from './pages/Jobs'
 import JobForm from './pages/JobForm'
+import Navbar from './components/Navbar'
+
+function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      <main>
+        {children}
+      </main>
+    </>
+  );
+}
 
 function App() {
 
@@ -11,9 +23,9 @@ function App() {
     <Routes>
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
-      <Route path='/dashboard' element={<Dashboard />} />
-      <Route path='/jobs' element={<Jobs />} />
-      <Route path='/jobs/new' element={<JobForm />} />
+      <Route path='/dashboard' element={<Layout><Dashboard /></Layout>} />
+      <Route path='/jobs' element={<Layout><Jobs /></Layout>} />
+      <Route path='/jobs/new' element={<Layout><JobForm /></Layout>} />
     </Routes>
   )
 }
